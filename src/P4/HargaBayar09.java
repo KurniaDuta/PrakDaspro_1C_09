19c359c61d710f74e7f2ac6d05ca822e5b474a76
Duta Kurnia A <dutaka2945@gmail.com>
2023-09-20 12:44:36 +0700

    P4_percobaan 3_pertanyaan 2_09

--- src/P4/HargaBayar09.java
+++ src/P4/HargaBayar09.java
@@ -6,14 +6,25 @@ public class HargaBayar09 {
     public static void main(String[] args) {
         Scanner input09 = new Scanner(System.in);
 
-        int harga, jumlah;
-        double dis=0.1, total, bayar, jmlDis;
+        int harga, jumlah, halbuku;
+        String merk;
+        double dis, total, bayar, jmlDis;
+
+        System.out.println("Masukkan merk barang yang dibeli: ");
+        merk=input09.nextLine();
+
+        System.out.println("Masukkan jumlah halaman barang yang dibeli: ");
+        halbuku=input09.nextInt();
 
         System.out.println("Masukkan harga barang yang dibeli: ");
         harga=input09.nextInt();
 
         System.out.println("Masukkan Jumlah barang yang dibeli: ");
         jumlah=input09.nextInt();
+
+        System.out.println("Masukkan diskon barang yang dibeli: ");
+        dis=input09.nextDouble();
+
 
         total=harga*jumlah;
         jmlDis= total*dis;
