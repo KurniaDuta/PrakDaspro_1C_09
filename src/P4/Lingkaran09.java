b5ae2ad9c83ee13441ec5ba2c2e764352f02e07c
Duta Kurnia A <dutaka2945@gmail.com>
2023-09-20 09:14:19 +0700

    P4_percobaan 1_09

---
+++ src/P4/Lingkaran09.java
@@ -0,0 +1,26 @@
+package P4;
+
+import java.util.Scanner;
+
+/**
+ * Lingkaran09
+ */
+public class Lingkaran09 {
+
+    public static void main(String[] args) {
+        Scanner input09 = new Scanner(System.in);
+
+        int r;
+        double keliling, luas;
+
+        System.out.println("Maasukkan jari-jari linbgkaran: ");
+        r =  input09.nextInt();
+
+        keliling = 2*3.14*r;
+        luas = 3.14*r*r;
+
+        System.out.println(keliling);
+        System.out.println(luas);
+
+    }
+}
