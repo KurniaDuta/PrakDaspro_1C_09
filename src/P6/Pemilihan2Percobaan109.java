20831cfc53b2e5ee213376db369af30bf6df768f
Duta Kurnia A <dutaka2945@gmail.com>
2023-10-04 09:21:32 +0700

    P6_percobaan 1_09

---
+++ src/P6/Pemilihan2Percobaan109.java
@@ -0,0 +1,23 @@
+package P6;
+
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan109 {
+    public static void main(String[] args) {
+    Scanner input09 = new Scanner(System.in);
+    
+    int tahun;
+    System.out.print("Masukkan Tahun: ");
+    tahun = input09.nextInt();
+    
+    if (tahun % 4 == 0) {
+        if (tahun % 100 !=0) {
+            System.out.println("Tahun " + tahun + " adalah tahun kabisat");   
+        } 
+    } 
+    else {
+        System.out.println("Tahun " + tahun + " bukan tahun kabisat");
+    }
+    
+    }
+}
