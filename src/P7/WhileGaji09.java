e60644193136992d92e3fb0cd46d61cf2675485d
Duta Kurnia A <dutaka2945@gmail.com>
2023-10-11 11:12:26 +0700

    P7_percobaan 2_09

---
+++ src/P7/WhileGaji09.java
@@ -0,0 +1,42 @@
+package P7;
+
+import java.util.Scanner;
+
+public class WhileGaji09 {
+    public static void main(String[] args) {
+        Scanner input09 = new Scanner(System.in);
+
+        int jumlahKaryawan, jumlahJamLembur;
+        double gajiLembur = 0, totalGajiLembur = 0;
+        String jabatan;
+
+        System.out.print("Masukkan jumlah karyawan: ");
+        jumlahKaryawan = input09.nextInt();
+
+        int i = 0;
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.print("Masukkan jabatan ke-" + (i + 1) + ": ");
+            jabatan = input09.next();
+            System.out.println("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = input09.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("Direktur")) {
+                continue;
+            }
+            else if (jabatan.equalsIgnoreCase("manajer")) {
+                gajiLembur = jumlahJamLembur * 100_000;
+            }
+            else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajiLembur = jumlahJamLembur * 75_000;
+            }
+
+            totalGajiLembur += gajiLembur;
+        }
+        System.out.println("Total gaji lembur: " + totalGajiLembur);
+
+        input09.close();
+
+    }
+}
