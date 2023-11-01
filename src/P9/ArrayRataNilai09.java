c2f176cc9bb3545a54d98f625785811c346ea425
Duta Kurnia A <dutaka2945@gmail.com>
2023-11-01 11:22:38 +0700

    P9_percobaan 3_pertanyaan 2_09

--- src/P9/ArrayRataNilai09.java
+++ src/P9/ArrayRataNilai09.java
@@ -6,24 +6,29 @@ public class ArrayRataNilai09 {
     public static void main(String[] args) {
         Scanner input09 = new Scanner(System.in);
 
-        int[] nilaiMHS = new int[10];
-        int mhsLulus = 0;
-        double total = 0, average;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMHS = input09.nextInt();
+        input09.nextLine();
 
-        for (int i = 0; i < nilaiMHS.length; i++) {
+        int mhsLulus = 0, mhsTidakLulus = 0;
+        int[] nilaiMHS = new int[jmlMHS];
+        double totalLulus = 0, totalTidakLulus = 0,
+                averageLulus = 0, averageTidakLulus = 0;
+
+        for (int i = 0; i < jmlMHS; i++) {
             System.out.print("Masukkan Nilai Ke-" + (i + 1) + ": ");
             nilaiMHS[i] = input09.nextInt();
             if (nilaiMHS[i] > 70) {
                 mhsLulus++;
+                totalLulus += nilaiMHS[i];
+                averageLulus = totalLulus / mhsLulus;
+            } else {
+                mhsTidakLulus++;
+                totalTidakLulus += nilaiMHS[i];
+                averageTidakLulus = totalTidakLulus / mhsTidakLulus;
             }
         }
-
-        for (int i = 0; i < nilaiMHS.length; i++) {
-            total += nilaiMHS[i];
-        }
-        average = total / nilaiMHS.length;
-        System.out.println("Rata-rata Nilai Mahasiswa = " + average);
-
-        System.out.println("Mahasiswa Lulus = " + mhsLulus);
+        System.out.printf("Rata-rata nilai lulus = %.1f\n", averageLulus);
+        System.out.printf("Rata-rata nilai tidak lulus = %.1f\n", averageTidakLulus);
     }
 }
