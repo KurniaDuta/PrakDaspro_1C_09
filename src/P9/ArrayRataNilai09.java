06f1ffe4ee66f8f77b02ebf233326cac6f5df130
Duta Kurnia A <dutaka2945@gmail.com>
2023-11-01 10:46:10 +0700

    P9_percobaan 3_pertanyaan 1_09

--- src/P9/ArrayRataNilai09.java
+++ src/P9/ArrayRataNilai09.java
@@ -7,17 +7,23 @@ public class ArrayRataNilai09 {
         Scanner input09 = new Scanner(System.in);
 
         int[] nilaiMHS = new int[10];
-        double total = 0;
-        double average;
+        int mhsLulus = 0;
+        double total = 0, average;
 
         for (int i = 0; i < nilaiMHS.length; i++) {
             System.out.print("Masukkan Nilai Ke-" + (i + 1) + ": ");
             nilaiMHS[i] = input09.nextInt();
+            if (nilaiMHS[i] > 70) {
+                mhsLulus++;
+            }
         }
+
         for (int i = 0; i < nilaiMHS.length; i++) {
             total += nilaiMHS[i];
         }
         average = total / nilaiMHS.length;
         System.out.println("Rata-rata Nilai Mahasiswa = " + average);
+
+        System.out.println("Mahasiswa Lulus = " + mhsLulus);
     }
 }
