48a03e7499c7c995ebf41bfa1fc8b7967ad5fc88
Duta Kurnia A <dutaka2945@gmail.com>
2023-11-08 16:53:01 +0700

    P10_percobaan 3_pertanyaan 1_09

--- src/P10/Numbers09.java
+++ src/P10/Numbers09.java
@@ -1,10 +1,16 @@
 package P10;
 
+import java.util.Arrays;
+
 public class Numbers09 {
     public static void main(String[] args) {
         int[][] myNummbers = new int[3][];
         myNummbers[0] = new int[5];
         myNummbers[1] = new int[3];
         myNummbers[2] = new int[1];
+
+        for (int i = 0; i < myNummbers.length; i++) {
+            System.out.println(Arrays.toString(myNummbers[i]));
+        }
     }   
 }
