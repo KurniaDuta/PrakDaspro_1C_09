7a26dd892691ae14a8e3881b6145828534f1ed96
Duta Kurnia A <dutaka2945@gmail.com>
2023-11-14 17:30:01 +0700

    P11_percobaan 1_09

---
+++ src/P11/Star09.java
@@ -0,0 +1,15 @@
+package P11;
+
+import java.util.Scanner;
+public class Star09 {
+    public static void main(String[] args) {
+        Scanner input09 = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int n = input09.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
+    }
+}
