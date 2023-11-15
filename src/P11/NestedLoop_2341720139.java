f940c6d7770c28b832274e5f071ef5232c1bcce4
Duta Kurnia A <dutaka2945@gmail.com>
2023-11-15 08:54:16 +0700

    P11_percobaan 3_pertanyaan 3_09

--- src/P11/NestedLoop_2341720139.java
+++ src/P11/NestedLoop_2341720139.java
@@ -9,7 +9,7 @@ public class NestedLoop_2341720139 {
         double temps[][] = new double[5][7];
 
         for (int i = 0; i < temps.length; i++) {
-            System.out.println("Kota ke-" + (i+1));
+            System.out.println("Kota ke-" + (i + 1));
             for (int j = 0; j < temps[i].length; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
                 temps[i][j] = input09.nextDouble();
@@ -20,13 +20,15 @@ public class NestedLoop_2341720139 {
         int kotaIndex = 1;
         for (double[] kota : temps) {
             System.out.print("Kota ke-" + kotaIndex++ + ": ");
-
+            double totalTemp = 0;
 
             for (double temperature : kota) {
                 System.out.print(temperature + " ");
-                
+                totalTemp += temperature;
             }
 
+            double averageTemp = totalTemp / kota.length;
+            System.out.printf("\nRata-rata suhu: %.1f", averageTemp);
 
             System.out.println();
         }
