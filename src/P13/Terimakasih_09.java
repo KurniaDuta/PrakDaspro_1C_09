e111dd3e2f467016bed8e49c6512715b2ca8f426
Duta Kurnia A <dutaka2945@gmail.com>
2023-11-29 11:42:45 +0700

    P13_percobaan 1_09

---
+++ src/P13/Terimakasih_09.java
@@ -0,0 +1,12 @@
+package P13;
+
+public class Terimakasih_09 {
+    public static void UcapanTerimakasih() {
+        System.out.println("Thank you for being the best teacher in the world.\n"
+                + "You inspired in me alove for learning and made me feel like I could ask you anything.");
+    }
+
+    public static void main(String[] args) {
+        UcapanTerimakasih();
+    }
+}
