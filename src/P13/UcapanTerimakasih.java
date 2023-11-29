aa5b0e7b1e6203bfe847ded3078c14c55c6557e3
Duta Kurnia A <dutaka2945@gmail.com>
2023-11-29 19:24:05 +0700

    P13_percobaan 4_pertanyaan 3_09

--- src/P13/UcapanTerimakasih.java
+++ src/P13/UcapanTerimakasih.java
@@ -10,15 +10,21 @@ public class UcapanTerimakasih {
         input09.close();
         return namaOrang;
     }
-    
+
     public static void UcapanTerimakasih() {
         String nama = PenerimaUcapan();
         System.out.println("Thank you " + nama + " for being the best teacher in the world.\n"
                 + "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
 
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
+
     public static void main(String[] args) {
         UcapanTerimakasih();
+        String ungkapan = "Once Again, Thank you to my teacher";
+        UcapanTambahan(ungkapan);
     }
 
     
