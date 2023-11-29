6a1e1c1d6f98f8257aa151ae62ccd1ad54a3f9e6
Duta Kurnia A <dutaka2945@gmail.com>
2023-11-29 11:23:20 +0700

    Twinkle Little Star

---
+++ src/P13/LirikLagu.java
@@ -0,0 +1,15 @@
+package P13;
+public class LirikLagu {
+    public static void main(String[] args) {
+        lirikLagu(args);
+        System.out.println("Up above the world so high");
+        System.out.println("Like a diamond in the sky");
+        lirikLagu(args);
+    }
+
+    static void lirikLagu(String[] lirik){
+        System.out.println("Twinkle twinkle little star");
+        System.out.println("How i wonder what you are");
+    }
+
+}
