828b2ecd08ebb80d61838fa0fdb962394adf058e
Duta Kurnia A <dutaka2945@gmail.com>
2023-11-29 19:38:23 +0700

    P13_percobaan 5_09_perbaikan

--- src/P13/Percobaan5_09.java
+++ src/P13/Percobaan5_09.java
@@ -1,6 +1,6 @@
 package P13;
 
-public class Percobaan5 {
+public class Percobaan5_09 {
     static void Tampil(String str, int... a) {
         System.out.println("String: " + str);
         System.out.println("Jumlah argumen/paramater: " + a.length);
